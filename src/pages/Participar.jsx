4120cf1985c1498263f687682242faf5032fa802
Jhonattan Rivera <jrivera@atiksoluciones.com>
2025-11-22 21:43:43 +0000

    File changes

--- src/pages/Participar.jsx
+++ src/pages/Participar.jsx
@@ -7,6 +7,7 @@ import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { createPageUrl } from "../utils";
 
 export default function Participar() {
   const queryClient = useQueryClient();
