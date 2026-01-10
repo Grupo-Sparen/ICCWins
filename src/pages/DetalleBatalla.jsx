35dabbf177160541024da6a41b88cfd03cc5a5df
Jhonattan Rivera <jrivera@atiksoluciones.com>
2026-01-10 17:11:18 +0000

    File changes

--- src/pages/DetalleBatalla.jsx
+++ src/pages/DetalleBatalla.jsx
@@ -171,7 +171,7 @@ export default function DetalleBatalla() {
   );
 
   return (
-    <div className="min-h-screen py-20">
+    <div className="min-h-screen pt-40 pb-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid lg:grid-cols-3 gap-8">
           {/* Main Content */}
